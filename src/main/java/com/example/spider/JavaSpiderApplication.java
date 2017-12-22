6176a82122a505536b976e89ab0d818c42e8690d
wangnan <nan.wang@htouhui.com>
2017-12-22 11:15:05 +0800

    INIT

---
+++ src/main/java/com/example/spider/JavaSpiderApplication.java
@@ -0,0 +1,12 @@
+package com.example.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpiderApplication.class, args);
+	}
+}
