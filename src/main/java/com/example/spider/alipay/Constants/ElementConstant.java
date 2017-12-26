eb664e6bdc5aa02f77cda8d76cce7eeb663fff88
wangnan <nan.wang@htouhui.com>
2017-12-26 19:19:27 +0800

    添加新包

---
+++ src/main/java/com/example/spider/alipay/Constants/ElementConstant.java
@@ -0,0 +1,15 @@
+package com.example.spider.alipay.Constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WangNan
+ * Email：nan.wang@htouhui.com
+ * Date: 2017-12-26
+ * Time: 下午7:18
+ */
+public interface ElementConstant {
+
+
+
+}
