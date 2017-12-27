fa56eff9e5b00532f09d1d8115beefcf7613813f
wangnan <nan.wang@htouhui.com>
2017-12-27 13:42:19 +0800

    修改实体类

--- src/main/java/com/example/spider/JavaSpiderApplication.java
+++ src/main/java/com/example/spider/JavaSpiderApplication.java
@@ -1,14 +1,13 @@
 package com.example.spider;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.spider")
 public class JavaSpiderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpiderApplication.class, args);
 	}
+
 }
